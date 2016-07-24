a28e80692fb64eb2184ea15f05c15284d4d9bd12
Sławek Coval <vanter2x@gmail.com>
2016-07-24 08:06:49 +0100

    xzcxz

---
+++ Game.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace MemGame
+{
+    public class Game
+    {
+        public List<Button> Btn = new List<Button>();
+        byte count = 0;
+        Button help = new Button();
+        //public Game() { }
+        public Game(Grid grid)
+        {
+            foreach (Button b in grid.Children)
+                Btn.Add(b);
+
+        }
+        public void RandomNr()
+        {
+            List<int> number = new List<int>();
+            for (int i = 0; i < 21; i++) number.Add(i);
+            for (int i = 0; i < 21; i++) number.Add(i);
+            var ii = 0;
+            Random rand = new Random();
+            foreach (Button b in Btn)
+            {
+                ii = rand.Next(number.Count - 1);
+                b.Tag = number[ii];
+                Image x = new Image();
+                x.Source = "1.png";
+
+                number.RemoveAt(ii);
+                b.Content = b.Tag;
+                b.Content = x;
+            }
+        }
+        public void ClickButton(object sender, RoutedEventArgs e)
+        {
+            
+            Button x = new Button();
+            x = (Button)sender;
+            if(count == 0)
+            {
+                count += 1;
+                help = (Button)sender;
+                x.IsEnabled = false;  
+            }
+            else
+            {
+                count = 0;
+                System.Threading.Thread.Sleep(1000);
+                if (x.Tag.ToString() == help.Tag.ToString())
+                {
+                    //MessageBox.Show("sddsad");
+                    x.Visibility = Visibility.Hidden;
+                    help.Visibility = Visibility.Hidden;
+                    //System.Threading.Thread.Sleep(1000);
+
+                }
+                else help.IsEnabled = true;
+            }
+        }
+        
+            
+        }
+
+    }
+
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MemGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Game game;
+        public MainWindow()
+        {
+            InitializeComponent();
+            foreach (Button x in gdGame.Children)
+            {
+                x.Content = "x";
+                //x.Click += game.ClickButton();
+            }
+                
+            
+            game = new MemGame.Game(gdGame);
+            game.RandomNr();
+            ImageBrush xx = new ImageBrush(1.png);
+            foreach (Button x in gdGame.Children)
+            {
+                
+
+                x.Click += button1_Click;
+                x.Background= 
+            }
+
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            game.ClickButton(sender, e);
+        }
+    }
+}
