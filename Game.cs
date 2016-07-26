eef67c3f70c39af891c0298d28409d638cb88d69
Sławek Coval <vanter2x@gmail.com>
2016-07-26 21:11:00 +0100

    next changes

--- Game.cs
+++ Game.cs
@@ -11,49 +11,59 @@ namespace MemGame
 {
     public class Game
     {
-        public List<Button> Btn = new List<Button>();
-        byte count = 0;
+        public List<Button> Btn = new List<Button>(); //lista Buttons
+        private StackPanel stcPanel = new StackPanel();
         Button help = new Button();
+        bool count = false; // pomocnicza zmienna (czy jakis button jest wciśniety)
+        
         //public Game() { }
-        public Game(Grid grid)
+        public Game(Grid grid) //konstruktor przypisuje referencje z buttonow z mainWindow do Listy<Buttonów>
         {
             foreach (Button b in grid.Children)
                 Btn.Add(b);
 
         }
-        public void RandomNr()
+        public void RandomNr() // Metoda losuje podwójne liczby z zakresu 0-20 i przypisuje do Button.Taga
         {
-            List<int> number = new List<int>();
+            List<int> number = new List<int>(); //pomocnicza lista wypełniona podwójnymi liczbami 0-20
             for (int i = 0; i < 21; i++) number.Add(i);
             for (int i = 0; i < 21; i++) number.Add(i);
-            var ii = 0;
+            var ii = 0; // pomocnicza zmienna 
             Random rand = new Random();
             foreach (Button b in Btn)
             {
-                ii = rand.Next(number.Count - 1);
+                ii = rand.Next(number.Count - 1); // losowanie liczby z listy i przypisanie do Taga
                 b.Tag = number[ii];
                 //Image x = new Image();
                 //x.Source = "1.png";
 
-                number.RemoveAt(ii);
+                number.RemoveAt(ii); // usunięcie wylosowanej liczby z listy
                 b.Content = b.Tag;
                // b.Content = x;
             }
         }
-        public void ClickButton(object sender, RoutedEventArgs e)
+        public void ClickButton(object sender, RoutedEventArgs e) //obsługa kliknięcia na przycisk
         {
+            // stworzenie dwóch pomocniczych buttonów i przypisanie do nich naciśniętych buttonów
             
             Button x = new Button();
             x = (Button)sender;
-            if(count == 0)
+            // obsługa zdarzenia
+            if (count == false)
             {
-                count += 1;
+                Image image = new Image();
+                image.Source = new System.Windows.Media.Imaging.BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "\\ico\\11.png", UriKind.Absolute));
+                x.Background = new System.Windows.Media.ImageBrush(image.Source);
+                count = true;
                 help = (Button)sender;
                 x.IsEnabled = false;  
             }
             else
             {
-                count = 0;
+                Image image = new Image();
+                image.Source = new System.Windows.Media.Imaging.BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "\\ico\\11.png", UriKind.Absolute));
+                x.Background = new System.Windows.Media.ImageBrush(image.Source);
+                count = false;
                 System.Threading.Thread.Sleep(1000);
                 if (x.Tag.ToString() == help.Tag.ToString())
                 {
@@ -64,11 +74,27 @@ namespace MemGame
 
                 }
                 else help.IsEnabled = true;
+
+                //System.Windows.Media.RadialGradientBrush ccc = new System.Windows.Media.RadialGradientBrush() {  }
+               // x.Background = new System.Windows.Media.r
             }
         }
-        
-            
+
+        public static System.Windows.Media.Imaging.BitmapImage LoadBitmapFromResource(string pathInApplication, System.Reflection.Assembly assembly = null)
+        {
+            if (assembly == null)
+            {
+                assembly = System.Reflection.Assembly.GetCallingAssembly();
+            }
+
+            if (pathInApplication[0] == '/')
+            {
+                pathInApplication = pathInApplication.Substring(1);
+            }
+            return new System.Windows.Media.Imaging.BitmapImage(new Uri(@"pack://application:,,,/" + assembly.GetName().Name + ";component/" + pathInApplication, UriKind.Absolute));
         }
+
+    }
 
     }
 
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,14 +25,30 @@ namespace MemGame
         public MainWindow()
         {
             InitializeComponent();
+            Style style = new Style();
+            Trigger changer = new Trigger() { Property = IsMouseOverProperty, Value = true };
+            //changer.SetValue(IsMouseOverProperty, true);
+            changer.Setters.Add(new Setter() { Property = OpacityProperty, Value = 0.2 });
+            style.Triggers.Add(changer);
+
+            
+
             foreach (Button x in gdGame.Children)
             {
                 x.Content = "x";
+                Image image = new Image();
+                image.Source = new System.Windows.Media.Imaging.BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "\\ico\\2.png", UriKind.Absolute));
+                x.Background = new System.Windows.Media.ImageBrush(image.Source);
+
                 //x.Click += game.ClickButton();
             }
-                
             
-            game = new MemGame.Game(gdGame);
+            
+            
+            //image1.Source = Game.LoadBitmapFromResource("Resources/2.png");
+            
+            //button1.LayoutTransform = image1;
+            game = new Game(gdGame);
             game.RandomNr();
             //Image xx = new Image();
             //xx.Source = ImageSource();
